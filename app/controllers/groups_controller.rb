a2db5ea405e294ddf086be4077c2a9ab2e3ee84f
chen <jingchen1214@gmail.com>
2017-04-01 13:35:51 +0800

    add validation to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,22 +17,28 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
+
+    if@group.save
       redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def update
     @group = Group.find(params[:id])
-    @group.update(group_params)
-
-    redirect_to groups_path, notice: "Update success"
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update success"
+    else
+      render :edit
+    end 
   end
 
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
       redirect_to groups_path, alert: "Moive deleted"
-  end 
+  end
 
   private
 
