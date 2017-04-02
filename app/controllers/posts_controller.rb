e662b8a21a214be69e19bb8b27e7892831d7afb3
chen <jingchen1214@gmail.com>
2017-04-02 23:31:30 +0800

    implement post's edit/update/destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,14 +23,32 @@ class PostsController < ApplicationController
     end
   end
 
-    def edit
-      @group = Group.find(params[:id])
-      @post.group = @group
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
 
-      if !current_user.is_member_of?(@group)
-        redirect_to root_path, alert: "You have no permission."
-      end
+    if !current_user.is_member_of?(@group)
+      redirect_to root_path, alert: "You have no permission."
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+      redirect_to account_posts_path, alert: "Moive deleted"
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Updated success"
+    else
+      render :edit
+    end
+  end
 
   private
 
